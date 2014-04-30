f41b3af79a2bcfb07d4f85270d7387f50af87513
Florian Schweikert <florian.schweikert@radarservices.com>
2014-04-30 13:35:41 +0200

    add testrun overview page

--- test_secretary/urls.py
+++ test_secretary/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'test_secretary.views.home', name='home'),
+    url(r'^testrun/(?P<rid>\d+)/?$', 'test_secretary.views.testrun_overview', name='testrun_overview'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- test_secretary/views.py
+++ test_secretary/views.py
@@ -7,3 +7,11 @@ def home(request):
     d['apps'] = Application.objects.all()
     d['testruns'] = TestRun.objects.all()
     return render(request, 'test_secretary/overview.html', d)
+
+def testrun_overview(request, rid):
+    d = {}
+    testrun = TestRun.objects.get(pk=rid)
+    d['testrun'] = testrun
+    d['testcaseruns'] = testrun.testcaserun_set.all()
+
+    return render(request, 'test_secretary/testrun_overview.html', d)
