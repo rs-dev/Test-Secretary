30df9f6da7baa4d92596674ec197204d697124b5
Florian Schweikert <florian.schweikert@radarservices.com>
2014-05-06 16:13:45 +0200

    basic view for new testrun creation

--- test_secretary/views.py
+++ test_secretary/views.py
@@ -1,3 +1,6 @@
+import re
+from datetime import datetime, date
+
 from django.shortcuts import render
 
 from .models import *
@@ -26,11 +29,30 @@ def testrun_overview(request, rid):
     return render(request, 'test_secretary/testrun_overview.html', d)
 
 
+TCREGEX = re.compile('testcase(?P<tcid>\d+)')
+
 def new_testrun(request):
     d = {}
     if request.method == 'POST':
-        # TODO
-        pass
+        name = request.POST.get('name', 'TestRun %s'% datetime.now().strftime('%c'))
+        comment = request.POST.get('comment', None)
+        version = request.POST.get('version', 'undefined')
+        # extract ids from post elements
+        for elem in request.POST.keys():
+            print(elem)
+        testcase_ids = [TCREGEX.match(elem).groupdict()['tcid'] for elem in request.POST if TCREGEX.match(elem)]
+        # get TestCases from ids
+        testcases = [TestCase.objects.get(pk=tcid) for tcid in testcase_ids]
+
+        if testcases:
+            print('Saving new TestRun: %s' % name)
+            testrun = TestRun(name=name, comment=comment, version=version)
+            testrun.save()
+            for testcase in testcases:
+                tcr = TestCaseRun(testcase=testcase, testrun=testrun)
+                tcr.save()
+        else:
+            d['errmsg'] = 'No testcases selected'
 
     d['apps'] = Application.objects.filter(active=True)
 
