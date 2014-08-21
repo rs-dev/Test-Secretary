7a95b426b1dd26e09d4924c594cf84e6c6383eee
Florian Schweikert <florian.schweikert@radarservices.com>
2014-08-21 14:40:33 +0200

    remove duplicated setting

--- test_secretary/settings.py
+++ test_secretary/settings.py
@@ -88,10 +88,6 @@ DATABASES = {
     }
 }
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -106,3 +102,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
