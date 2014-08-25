14a1f7c9808ba89d297369ed79419b064491a151
Florian Schweikert <florian.schweikert@radarservices.com>
2014-08-25 16:34:44 +0200

    add grappelli
    everything is better with grappelli

--- test_secretary/settings.py
+++ test_secretary/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,7 @@ INSTALLED_APPS = (
     'test_secretary',
     'unittester',
     'south',
-    #'django_extensions',
+    'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (
