2b67b0ebca80b5e14d2e5968219dca85917a01e8
Florian Schweikert <florian.schweikert@radarservices.com>
2014-09-16 16:25:14 +0200

    require login to run unittests

--- unittester/views.py
+++ unittester/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http.response import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 from test_secretary.models import *
 from unittester.models import *
 from unittester.tasks import run_testcaserun_tests
 
 
+@login_required
 def run_testcaserun(request, tcrid):
     testcaserun = get_object_or_404(TestCaseRun, pk=tcrid)
     unittests = testcaserun.testcase.unittest_set.all()
